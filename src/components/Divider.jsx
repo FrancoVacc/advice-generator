ef0c98d6edc9c7666109c00002dadcf76ceabfdd
Franco Vaccari <francovacc2.0@gmail.com>
2023-10-13 16:50:57 -0300

    Finished 13-10-2023

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -1,13 +1,15 @@
 import mobilepattern from "../assets/pattern-divider-mobile.svg";
 import desktoppattern from "../assets/pattern-divider-desktop.svg";
 
+const screenWidth = window.screen.width;
+console.log(screenWidth);
+
 const Divider = () => {
-  return (
-    <>
-      <img src={mobilepattern} alt="" />
-      {/* <img src={desktoppattern} alt="" /> */}
-    </>
-  );
+  if (screenWidth > 425) {
+    return <img src={desktoppattern} alt="" />;
+  }
+
+  return <img src={mobilepattern} alt="" />;
 };
 
 export default Divider;
